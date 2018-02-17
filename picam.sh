98e0b25228b4f937eeb0815a125e9e85d0ba2d99
Alexander Dobrynin <info@alex-dobrynin.com>
2018-02-17 17:08:00 +0100

    enhanced picam.sh

--- picam.sh
+++ picam.sh
@@ -1,2 +1,19 @@
-sudo bash /home/pi/make_dirs.sh
-sudo /home/pi/picam/picam -o /run/shm/hls --volume 5.0 --channels 2 --audiobitrate 96000 --vfr --avclevel 3.1 --autoex --time --alsadev hw:1,0 >/var/log/picam.log 2>&1 &
+#! /bin/sh
+
+case "$1" in
+    start)
+        echo "Starting Picam"
+        sudo bash /home/pi/make_dirs.sh
+        sudo /home/pi/picam/picam -o /run/shm/hls --volume 5.0 --channels 2 --audiobitrate 96000 --vfr --avclevel 3.1 --autoex --time --alsadev hw:1,0 >/var/log/picam.log 2>&1 &
+    ;;
+    stop)
+        echo "Stopping Picam"
+        killall picam
+    ;;
+    *)
+        echo "Usage: picam {start|stop}"
+        exit 1
+    ;;
+esac
+
+exit 0
