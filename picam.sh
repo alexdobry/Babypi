e31a55fc7155395b0ba396ebaba264f8ba1726d1
Alexander Dobrynin <info@alex-dobrynin.com>
2018-02-18 17:05:51 +0100

    picam.sh now returns state

--- picam.sh
+++ picam.sh
@@ -1,19 +1,20 @@
-#! /bin/sh
+# !/bin/sh
+
+nginx_path=/home/pi/Babypi/nginx.sh
 
 case "$1" in
     start)
         echo "Starting Picam"
-        sudo bash /home/pi/make_dirs.sh
+	sudo $nginx_path start
+        sudo /home/pi/make_dirs.sh
         sudo /home/pi/picam/picam -o /run/shm/hls --volume 5.0 --channels 2 --audiobitrate 96000 --vfr --avclevel 3.1 --autoex --time --alsadev hw:1,0 >/var/log/picam.log 2>&1 &
-    ;;
+	return $? ;;
     stop)
         echo "Stopping Picam"
+	sudo $nginx_path stop
         killall picam
-    ;;
+	return $? ;;
     *)
         echo "Usage: picam {start|stop}"
-        exit 1
-    ;;
+        return 1 ;;
 esac
-
-exit 0
