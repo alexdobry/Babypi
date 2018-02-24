e05b1610e0f6840b18a4f5beed79caaeaefa1886
Alexander Dobrynin <info@alex-dobrynin.com>
2018-02-24 13:40:56 +0100

    added shell script for light

---
+++ light.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+light_pin=2
+gpio -g mode $light_pin out
+
+function on {
+	gpio -g write $light_pin 1
+}
+
+function off {
+        gpio -g write $light_pin 0
+}
+
+function status {
+	gpio -g read $light_pin
+}
+
+case $1 in  
+  on) on ;; 
+  off) off ;; 
+  status) status ;; 
+esac
