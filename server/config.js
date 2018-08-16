a3ff1d32e6dc34cf32b5afd2ab8191cd06e7924b
Alexander Dobrynin <info@alex-dobrynin.com>
2018-08-16 15:10:41 +0200

    dotenv bug fixes

--- server/config.js
+++ server/config.js
@@ -2,10 +2,10 @@ const env = process.env.NODE_ENV || 'dev'
 
 const dev = {
   apns: {
-    key: process.env.KEY || '',
-    keyId: process.env.KEY_ID || '',
-    teamId: process.env.TEAM_ID || '',
-    bundleId: process.env.BUNDLE_ID || ''
+    key: process.env.KEY,
+    keyId: process.env.KEY_ID,
+    teamId: process.env.TEAM_ID,
+    bundleId: process.env.BUNDLE_ID
   }
 }
 
